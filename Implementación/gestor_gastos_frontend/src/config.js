2c65814f06c1c46b52d935b432ced3c21a287738
Jeremías Micheloud <jeremiche20@gmail.com>
2025-07-29 18:47:16 -0300

    commit final deploy

--- Implementación/gestor_gastos_frontend/src/config.js
+++ Implementación/gestor_gastos_frontend/src/config.js
@@ -1,5 +1,6 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
 
-console.log('API_URL configurada:', API_URL); // Para debug
+const API_URL = import.meta.env.VITE_API_URL?.replace(/\/$/, '') || 'http://localhost:3000';
+
+console.log('API_URL configurada:', API_URL);
 
 export { API_URL };
